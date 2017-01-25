1cc10a9142e69aac46ee478a4cb2169d3355b554
railsroger <railsroger@gmail.com>
2017-01-25 05:15:20 -0800

    Add Validates. Add tests to product_test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.delete_all
 # . . .
-Product.create(title: 'Seven Mobile Apps in Seven Weeks',
+Product.create(title: 'Waking Up Chase: One Students Journey to Awakening His Potentia ',
 description:
-%{<p>
-<em>Native Apps, Multiple Platforms</em>
-Answer the question “Can we build this for ALL the devices?” with a
-resounding YES. This book will help you get there with a real-world
-introduction to seven platforms, whether you’re new to mobile or an
-experienced developer needing to expand your options. Plus, you’ll find
-out which cross-platform solution makes the most sense for your needs.
-</p>},
+%{After a wild night partying with some newly acquainted college friends, Chase awakens in a place that is not his dorm room and is not sure who he is. In this invigorating journey of trying to not only retrace his steps from the night before but the four years that have gone by, Chase realizes his life will never be the same again. },
+image_url: '7book.jpg',
+price: 6.93)
+Product.create(title: 'Devastated Lands: Part 1: A Post-Apocalyptic Thriller ',
+description:
+%{Lahars from an erupting Mount Rainier in Washington devastate the Puyallup Valley and threaten the millions of people who live in Seattle and Tacoma. Caught up in the natural catastrophe is Shane Cooper, a mountain guide from Telluride, Colorado, and Mikaela Brand, a young mixed martial arts (MMA) competitor. },
+image_url: '6book.jpg',
+price: 0.99)
+Product.create(title: 'Hillbilly Elegy: A Memoir of a Family and Culture in Crisis',
+description:
+%{From a former marine and Yale Law School graduate, a powerful account of growing up in a poor Rust Belt town that offers a broader, probing look at the struggles of America’s white working class Hillbilly Elegy is a passionate and personal analysis of a culture in crisis—that of white working-class Americans. The decline of this group, a demographic of our country that has been slowly disintegrating over forty years, has been reported on with growing frequency and alarm, but has never before been written about as searingly from the inside. J. D. Vance tells the true story of what a social, regional, and class decline feels like when you were born with it hung around your neck. },
 image_url: '5book.jpg',
-price: 26.00)
+price: 15.95)
 # . . .
